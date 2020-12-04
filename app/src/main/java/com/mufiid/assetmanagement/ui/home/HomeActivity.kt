392b8bed2bc47cb71f1b4e9e363ebe7ba0361cac
imufiid <imufiid98@gmail.com>
2020-12-04 09:30:47 +0700

    setting recycler view for show list data

--- app/src/main/java/com/mufiid/assetmanagement/ui/home/HomeActivity.kt
+++ app/src/main/java/com/mufiid/assetmanagement/ui/home/HomeActivity.kt
@@ -1,24 +1,94 @@
 package com.mufiid.assetmanagement.ui.home
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mufiid.assetmanagement.R
+import com.mufiid.assetmanagement.adapter.AssetAdapter
+import com.mufiid.assetmanagement.models.Asset
 import com.mufiid.assetmanagement.ui.addupdate.AddUpdateActivity
+import com.mufiid.assetmanagement.ui.detail.DetailActivity
+import com.mufiid.assetmanagement.utils.Constants
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var adapter: AssetAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
         initSupportActionBar()
+        init()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setRecyclerView()
+        showData()
     }
 
     private fun init() {
-        TODO("Not yet implemented")
+        homeViewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.NewInstanceFactory()
+        ).get(HomeViewModel::class.java)
+
+        homeViewModel.getState().observer(this, Observer {
+            handleUIState(it)
+        })
+    }
+
+    private fun handleUIState(it: AssetState?) {
+        when(it) {
+            is AssetState.IsLoading -> showLoading(it.state)
+        }
+    }
+
+    private fun showLoading(state: Boolean) {
+        if(state) {
+            shimmer_container_asset.visibility = View.VISIBLE
+            shimmer_container_asset.startShimmer()
+        } else {
+            shimmer_container_asset.stopShimmer()
+            shimmer_container_asset.visibility = View.GONE
+        }
+    }
+
+    private fun setRecyclerView() {
+        adapter = AssetAdapter { asset ->
+            showSelectedData(asset)
+        }.apply {
+            notifyDataSetChanged()
+        }
+
+        rv_data.layoutManager = LinearLayoutManager(this)
+        rv_data.adapter = adapter
+    }
+
+    private fun showData() {
+        Constants.getUserData(this)?.let {
+            homeViewModel.getAllData(it.id, it.token)
+        }
+        homeViewModel.getAssets().observe(this, Observer {
+            if(it != null) {
+                adapter.setAsset(it)
+            }
+        })
+    }
+
+    private fun showSelectedData(asset: Asset) {
+        startActivity(Intent(this, DetailActivity::class.java).apply {
+            putExtra(DetailActivity.DATA_ASSET, asset)
+        })
     }
 
     private fun initSupportActionBar() {
@@ -34,7 +104,7 @@ class HomeActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId) {
+        return when (item.itemId) {
             R.id.add -> {
                 startActivity(Intent(this, AddUpdateActivity::class.java))
                 true
--- app/src/main/java/com/mufiid/assetmanagement/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/mufiid/assetmanagement/ui/home/HomeViewModel.kt
@@ -1,11 +1,62 @@
 package com.mufiid.assetmanagement.ui.home
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.mufiid.assetmanagement.api.ApiClient
+import com.mufiid.assetmanagement.models.Asset
+import com.mufiid.assetmanagement.utils.SingleLiveEvent
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class HomeViewModel : ViewModel() {
 
+    private var assets = MutableLiveData<List<Asset>>()
+    private var asset = MutableLiveData<Asset>()
 
-    fun setData() {}
+    private var state: SingleLiveEvent<AssetState> = SingleLiveEvent()
 
-    fun getData() {}
+    private var api = ApiClient.getInstance()
+
+    fun getAllData(user_id: String?, token: String?) {
+        state.value = AssetState.IsLoading(true)
+        CompositeDisposable().add(
+            api.getData(token, user_id)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    when(it.status) {
+                        200 -> assets.postValue(it.data)
+                        else -> state.value = AssetState.Error(it.message)
+                    }
+                    state.value = AssetState.IsLoading()
+                }, {
+                    state.value = AssetState.Error(it.message)
+                    state.value = AssetState.IsLoading()
+                })
+        )
+    }
+
+    fun getDataById(user_id: Int?, idAsset: Int?) {}
+
+    fun getAssets() = assets
+    fun getAsset() = asset
+    fun getState() = state
+}
+
+sealed class AssetState() {
+    data class ShowToast(var message: String?): AssetState()
+    data class IsLoading(var state: Boolean = false): AssetState()
+    data class Error(var err: String?): AssetState()
+    data class IsSuccess(var what: Int? = null): AssetState()
+    data class AssetValidation(
+        var plat_mobil: String? = null,
+        var merk_mobil: String? = null,
+        var no_rangka: String? = null,
+        var no_mesing: String? = null,
+        var owner_name: String? = null,
+        var date_oil: String? = null,
+        var next_date_oil: String? = null
+    ): AssetState()
+    object Reset: AssetState()
 }
