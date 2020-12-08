a60632da1ee4bd51c1eaf1963bdd7986f008bfcb
imufiid <imufiid98@gmail.com>
2020-12-08 08:50:49 +0700

    get response date from api

--- app/src/main/java/com/mufiid/assetmanagement/ui/addupdate/AddUpdateActivity.kt
+++ app/src/main/java/com/mufiid/assetmanagement/ui/addupdate/AddUpdateActivity.kt
@@ -1,20 +1,150 @@
 package com.mufiid.assetmanagement.ui.addupdate
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.view.View
+import android.widget.DatePicker
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.mufiid.assetmanagement.R
+import com.mufiid.assetmanagement.models.Asset
+import kotlinx.android.synthetic.main.activity_add_update.*
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
-class AddUpdateActivity : AppCompatActivity() {
+
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+class AddUpdateActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
+    View.OnClickListener {
+
+    companion object {
+        const val DATA = "data_asset"
+        const val IS_UPDATE = "is_update"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_update)
 
         initSupportActionBar()
+        setParcelable()
+        init()
+
+    }
+
+    private fun init() {
+        et_date_oil.setOnFocusChangeListener { _, b ->
+            if (b) {
+                showCalendar()
+            }
+        }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun setParcelable() {
+        val data = intent.getParcelableExtra<Asset>(DATA)
+        val isUpdate = intent.getBooleanExtra(IS_UPDATE, false)
+        val outputDateOil: String
+        val outputNextDateOil: String
+
+        if (isUpdate) {
+            data?.let {
+                et_plat_mobil.setText(it.platMobil)
+                et_merk_mobil.setText(it.merkMobil)
+                et_no_mesin.setText(it.noMesin)
+                et_no_rangka.setText(it.noRangka)
+                et_owner_name.setText(it.ownerName)
+
+//                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+//                    val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+//                    val date = LocalDate.parse(it.dateOil)
+//                    outputDateOil = formatter.format(date)
+//
+//                    val date2 = LocalDate.parse(it.dateExpiredOil)
+//                    outputNextDateOil = formatter.format(date2)
+//
+//                } else {
+//                    val formatter = SimpleDateFormat("dd-MM-yyyy")
+//
+//                    val parser = SimpleDateFormat("yyyy-MM-dd")
+//                    outputDateOil = formatter.format(parser.parse(it.dateOil))
+//
+//                    val parser2 = SimpleDateFormat("yyyy-MM-dd")
+//                    outputNextDateOil = formatter.format(parser2.parse(it.dateOil))
+//                }
+
+                et_date_oil.apply {
+                    isEnabled = false
+                    setText(it.dateOil)
+                }
+                et_next_date_oil.setText(it.dateExpiredOil)
+            }
+        } else {
+
+            val date = Calendar.getInstance()
+            val currentDate = date.time
+
+            date.add(Calendar.MONTH, +4)
+            val next4Date = date.time
+
+            val format = SimpleDateFormat("dd-MM-yyyy")
+            outputDateOil = format.format(currentDate)
+            outputNextDateOil = format.format(next4Date)
+
+            et_date_oil.apply {
+                setText(outputDateOil)
+            }
+
+            et_next_date_oil.apply {
+                setText(outputNextDateOil)
+            }
+        }
     }
 
     private fun initSupportActionBar() {
         supportActionBar?.let {
             it.setDisplayHomeAsUpEnabled(true)
         }
+    }
+
+    override fun onNavigateUp(): Boolean {
+        Toast.makeText(this, "Back", Toast.LENGTH_SHORT).show()
+        return super.onNavigateUp()
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    override fun onDateSet(v: DatePicker?, year: Int, month: Int, day: Int) {
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, day)
+        val date = calendar.time
+
+        val dateFormat = SimpleDateFormat("dd-MM-yyyy")
+        val dateStr = dateFormat.format(date)
+        et_date_oil.setText(dateStr)
+
+        calendar.add(Calendar.MONTH, +4)
+        val nextDate = calendar.time
+        val nextDateStr = dateFormat.format(nextDate)
+        et_next_date_oil.setText(nextDateStr)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+        }
+    }
+
+    private fun showCalendar() {
+        val calender = Calendar.getInstance()
+        val datePick = DatePickerDialog(
+            this, this,
+            calender.get(Calendar.YEAR),
+            calender.get(Calendar.MONTH),
+            calender.get(Calendar.DAY_OF_MONTH)
+        )
+
+        datePick.show()
     }
 }
--- app/src/main/java/com/mufiid/assetmanagement/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/mufiid/assetmanagement/ui/detail/DetailActivity.kt
@@ -1,20 +1,98 @@
 package com.mufiid.assetmanagement.ui.detail
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.mufiid.assetmanagement.R
+import com.mufiid.assetmanagement.models.Asset
+import com.mufiid.assetmanagement.ui.addupdate.AddUpdateActivity
+import kotlinx.android.synthetic.main.activity_detail.*
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
-class DetailActivity : AppCompatActivity() {
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+class DetailActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var dataAsset: Asset? = null
+    companion object {
+        var DATA_ASSET = "data_asset"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
         initSetSupportActionBar()
+        init()
+        setParcelable()
+
+    }
+
+    private fun init() {
+        btn_edit.setOnClickListener(this)
+        btn_delete.setOnClickListener(this)
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun setParcelable() {
+        val outputDateOil: String
+        val outputNextDateOil: String
+
+        dataAsset = intent.getParcelableExtra<Asset>(DATA_ASSET)
+        dataAsset?.let {
+            tv_plat_mobil.text = it.platMobil
+            tv_merk_mobil.text = it.merkMobil
+            tv_no_rangka.text = it.noRangka
+            tv_no_mesin.text = it.noMesin
+            tv_owner_name.text = it.ownerName
+
+//            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+//                val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+//                val date = LocalDate.parse(it.dateOil)
+//                outputDateOil = formatter.format(date)
+//
+//                val date2 = LocalDate.parse(it.dateExpiredOil)
+//                outputNextDateOil = formatter.format(date2)
+//
+//            } else {
+//                val formatter = SimpleDateFormat("dd-MM-yyyy")
+//
+//                val parser = SimpleDateFormat("yyyy-MM-dd")
+//                outputDateOil = formatter.format(parser.parse(it.dateOil))
+//
+//                val parser2 = SimpleDateFormat("yyyy-MM-dd")
+//                outputNextDateOil = formatter.format(parser2.parse(it.dateOil))
+//            }
+
+            tv_ganti_oli_terakhir.text = it.dateOil
+            tv_ganti_oli_berikutnya.text = it.dateExpiredOil
+        }
     }
 
     private fun initSetSupportActionBar() {
         supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
             it.title = getString(R.string.detail)
+        }
+    }
+
+    override fun onNavigateUp(): Boolean {
+        finish()
+        return super.onNavigateUp()
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.btn_edit -> {
+                startActivity(Intent(this, AddUpdateActivity::class.java).apply {
+                    putExtra(AddUpdateActivity.DATA, dataAsset)
+                    putExtra(AddUpdateActivity.IS_UPDATE, true)
+                })
+            }
+            R.id.btn_delete -> {}
         }
     }
 }
